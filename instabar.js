3bb1bef0b9359d10f90fbf2936996c4fd0a29ce3
avcreation <dev@avcreation.fr>
2013-07-25 13:00:53 +0200

    First release of Instabar.js

---
+++ instabar.js
@@ -0,0 +1,112 @@
+/*===================================
+= InstaBar.js                       =
+= GPL2 licence                      =
+= Created by Alexandre Voiney       =
+= V.1 - juillet 2013                =
+===================================*/
+
+function Instabar(options) {
+  // Default parameters
+  this.parameters={
+    "access_token": "",
+    "selector": "instabar",
+    'quality': "thumbnail",
+    'user_id': "",
+  };
+
+  // Override defaults
+  for(param in options) {
+    this.parameters[param] = options[param]
+  }
+
+  // Create a unique ID for each instance
+  this.instance_id = this._getUniqueID();
+}
+
+Instabar.prototype = {
+  _getUniqueID: function() {
+    var text = "";
+    var choices = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += choices.charAt(Math.floor(Math.random() * choices.length));
+
+    return text;
+  },
+  display: function(res){
+    // Check success or not
+    if(res.meta.code == 200) {
+      // Get the HTML element wanted by the user
+      var container = document.getElementById(this.parameters.selector);
+
+      // Create the 'Instabar' element
+      var insta_bar = document.createElement("div");
+      insta_bar.style.textAlign = "center";
+      // Fill the container with it
+      container.appendChild(insta_bar);
+
+      // Fetch the result
+      var data = res.data;
+      for(i=0 ; i < data.length ; ++i) {
+        // Create the wrapping link of the img HTML element
+        var image_a = document.createElement("a");
+        image_a.href = data[i].link;
+        image_a.target = "_blank";
+
+        // Create the img HTML element
+        var image_img = document.createElement("img");
+        image_img.src = data[i].images[this.parameters.quality].url;;
+        image_img.className = "insta-pict";
+
+        // Wrap the image with the link
+        image_a.appendChild(image_img);
+
+        // Fill the 'Instabar' with the link
+        insta_bar.appendChild(image_a);
+      }
+    }else if(res.meta.code == 400) {
+      // Create an error element
+      var _alert = document.createElement('span');
+      _alert.innerHTML = res.meta.error_message;
+      _alert.className = "alert";
+      // Append the element to the body
+      document.body.appendChild(_alert);
+      // Wait 3sec and add a class for CSS3 transition
+      setTimeout(function() {
+        _alert.className += " fading";
+      }, 3000);
+      // Delete this 'Instabar' instance
+      var instance = "instabar_"+this.instance_id;
+      delete window[instance];
+    }else {
+      // Create an unknown error element
+      var _alert = document.createElement('span');
+      _alert.innerHTML = "An unknown error has occured";
+       _alert.className = "alert";
+      // Append the element to the body
+      document.body.appendChild(_alert);
+      // Wait 3sec and add a class for CSS3 transition
+      setTimeout(function() {
+        _alert.className += " fading";
+      }, 3000);
+      // Delete this 'Instabar' instance
+      var instance = "instabar_"+this.instance_id;
+      delete window[instance];
+    }
+  },
+  run: function() {
+    // Duplicate the instance for callback reason
+    var instance = "instabar_"+this.instance_id;
+    window[instance] = new Instabar(this.parameters);
+    window[instance].instance_id = this.instance_id;
+    // Create the url
+    var url = "https://api.instagram.com/v1/users/self/feed/?access_token=" + this.parameters.access_token + "&callback="+instance+".display";
+
+    // Create a script element to avoid Cross-Domain ajax request
+    script = document.createElement('script');
+    script.src = url;
+    // Append the script to the end of the body element
+    body = document.getElementsByTagName('body');
+    body[0].appendChild(script);
+  }
+}
